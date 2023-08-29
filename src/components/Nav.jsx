9cbdb9fa096fdce476b7b1262780c12bd3e360db
Ajay Thanki <akthanki5@gmail.com>
2023-08-29 19:24:45 +0530

    Created Navigation Menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,47 @@
+import { headerLogo } from "../assets/images";
+import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants";
+import { useState } from "react";
 const Nav = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   return (
-    <div>Nav</div>
-  )
-}
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="logo" width={130} height={29} />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((navLink) => (
+            <li className="" key={navLink.label}>
+              <a
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+                href={navLink.href}
+              >
+                {navLink.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="hidden max-lg:block" onClick={()=>setIsMobileMenuOpen(!isMobileMenuOpen)}>
+          <img src={hamburger} alt="Menu" height={25} width={25} />
+          <div className={`${isMobileMenuOpen ? `flex opacity-100 transition-opacity duration-1000`: `hidden opacity-0`} w-full absolute bg-white top-0 mt-16 left-0 p-4`}>
+            <ul className="w-full flex flex-col justify-center items-center gap-8">
+              {navLinks.map((navLink) => (
+                <li className="" key={"mobile_" + navLink.label}>
+                  <a
+                    className="font-montserrat leading-normal text-lg text-slate-gray"
+                    href={navLink.href}
+                  >
+                    {navLink.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Nav
+export default Nav;
