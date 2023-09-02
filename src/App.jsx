e2e726c9d11d1a993aadf6fa99e83f0fde325308
Ajay Thanki <akthanki5@gmail.com>
2023-09-02 16:56:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,30 +14,30 @@ export default function App() {
   return (
     <main className="relative">
       <Nav />
-      <section className="xl:padding-1 wide:padding-r padding-b">
+      <section className="xl:padding-l wide:padding-r padding-b" id="home">
         <Hero/>
       </section>
-      <section className="padding">
+      <section className="padding" id="products">
         <PopularProducts/>
       </section>
-      <section className="padding">
+      <section className="padding" id="about-us">
         <SuperQuality/>
       </section>
-      <section className="padding-x py-10">
+      <section className="padding-x py-10" id="services">
         <Services/>
       </section>
-      <section className="padding">
+      <section className="padding" id="special-offers">
         <SpecialOffers/>
       </section>
-      <section className="bg-pale-blue padding">
+      <section className="bg-pale-blue padding" id="customer-reviews">
         <CustomerReviews/>
       </section>
-      <section className="padding-x sm:py-32 py-16 w-full">
+      <section className="padding-x sm:py-32 py-16 w-full" id="contact-us">
         <Subscribe/>
       </section>
-      <section className="bg-black padding-x padding-t pb-8">
+      <footer className="bg-black padding-x padding-t pb-8">
         <Footer/>
-      </section>
+      </footer>
     </main>
   );
 }
